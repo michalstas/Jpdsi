9fa91753b76ac22da97090d6a69a87c3110fed06
micha <micha@DESKTOP-JRR8BQ0>
2022-10-18 06:01:27 +0200

    kred_01

--- kred_01/src/com/jsfcourse/calc/KredBB.java
+++ kred_01/src/com/jsfcourse/calc/KredBB.java
@@ -2,44 +2,52 @@ package com.jsfcourse.calc;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 
+
 @Named
 @RequestScoped
 //@SessionScoped
 public class KredBB {
-	private String x;
-	private String y;
-	private String z;
+	private Double x;
+	private Double y;
+	private Double z;
 	private Double result;
+
+	
 
 	@Inject
 	FacesContext ctx;
 
-	public String getX() {
+	public Double getX() {
 		return x;
 	}
 
-	public void setX(String x) {
+	public void setX(Double x) {
 		this.x = x;
 	}
 
-	public String getY() {
+	public Double getY() {
 		return y;
 	}
 
-	public void setY(String y) {
+	public void setY(Double y) {
 		this.y = y;
 	}
 	
-	public String getZ() {
+	public Double getZ() {
 		return z;
 	}
 
-	public void setZ(String z) {
+	public void setZ(Double z) {
 		this.z = z;
 	}
 
@@ -53,12 +61,11 @@ public class KredBB {
 
 	public boolean doTheMath() {
 		try {
-			double x = Double.parseDouble(this.x);
-			double y = Double.parseDouble(this.y);
-			double z = Double.parseDouble(this.z);
 
-			result = x + (z * y)/100 * x;
 
+			result = (x + (z * y)/100 * x)/(y*12);
+			result = new BigDecimal(result).setScale(2, RoundingMode.HALF_UP).doubleValue();
+			
 			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Operacja wykonana poprawnie", null));
 			return true;
 		} catch (Exception e) {
